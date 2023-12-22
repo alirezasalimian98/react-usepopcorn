8fbb6ae2ba4ce1ffcd1765902f2d356c17242d2c
Alireza <alirezasalimiyan98@gmail.com>
2023-12-23 00:05:35 +0330

    working on star rating

--- src/StarRating.js
+++ src/StarRating.js
@@ -3,42 +3,75 @@ const containerStyle = { display: "flex", gap: "16px", alignItems: "center" };
 
 const starContainer = { display: "flex" };
 
-const textStyle = { lineHeight: "1", margin: "0" };
-
-export default function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(1);
+export default function StarRating({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+  className = "",
+  message = [],
+  defaultRating = 0,
+  onSetRating,
+}) {
+  const [rating, setRating] = useState(defaultRating);
+  const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
+    onSetRating(rating);
   }
+
+  const startStyle = {
+    heigh: `${size}px`,
+    width: `${size}px`,
+    display: "block",
+    cursor: "pointer",
+  };
+
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    fontSize: `${size / 1.5}px`,
+    color,
+  };
+
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainer}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <Star onRating={() => handleRating(i + 1)} full={rating >= i + 1} />
+          <Star
+            onRating={() => handleRating(i + 1)}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+            style={startStyle}
+            color={color}
+          />
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>
+        {message.length === maxRating
+          ? message[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
 }
 
-const startStyle = {
-  heigh: "48px",
-  width: "48px",
-  display: "block",
-  cursor: "pointer",
-};
-
-function Star({ onRating, full }) {
+function Star({ onRating, full, onHoverIn, onHoverOut, style, color }) {
   return (
-    <span style={startStyle} onClick={onRating}>
+    <span
+      role="button"
+      style={style}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+      onClick={onRating}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -47,7 +80,7 @@ function Star({ onRating, full }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { useState } from "react";
+
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color={"blue"} onSetRating={setMovieRating} />
+      <p>Movie rating is {movieRating}</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating maxRating={10} />
+    <StarRating maxRating={5} message={["a", "b", "c", "d", "d"]} />
+    <Test />
   </React.StrictMode>
 );
